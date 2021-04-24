34f6c3999902127186e116b6661ab6640ba5e433
jesskcunha <80522502+jesskcunha@users.noreply.github.com>
2021-04-23 21:41:15 -0300

    Update games.sql

--- games.sql
+++ games.sql
@@ -23,20 +23,20 @@ CREATE TABLE tb_personagem(
     FOREIGN KEY (classe_id) REFERENCES tb_classe(id)
 );
 
-INSERT INTO tb_classe(habilidade, int_forca, fraqueza) VALUES ("TELETRANSPORTE", "MEDIO", "TELETRANSPORTA SOMENTE P/ LUGARES QUE JA FOI");
+INSERT INTO tb_classe(habilidade, int_forca, fraqueza) VALUES ("TELETRANSPORTE", "MEDIO", "TELETRANSPORTA SOMENTE PARA LUGARES CONHECIDOS");
 INSERT INTO tb_classe(habilidade, int_forca, fraqueza) VALUES ("LASER PELOS OLHOS", "FORTE", "DOR FORTE NA CABEÇA AO LANÇAR MUITOS LASER");
 INSERT INTO tb_classe(habilidade, int_forca, fraqueza) VALUES ("EXPLOSÃO PELO TOQUE", "MEDIA", "NÃO TER CONTROLE AO TOQUE");
 INSERT INTO tb_classe(habilidade, int_forca, fraqueza) VALUES ("CONTROLE DA MENTE", "FORTE", "SÓ UMA POR VEZ");
 INSERT INTO tb_classe(habilidade, int_forca, fraqueza) VALUES ("PERSUASÃO", "FRACO", "PRECISA TOCAR A PESSOAS E CONVERSAR POR 5 MIN");
 
-INSERT INTO tb_personagem(nome, ataque, defesa, genero, classe_id) VALUES ("LUA", 3333.33, 2000.0, 'F', 4);
-INSERT INTO tb_personagem(nome, ataque, defesa, genero, classe_id) VALUES ("SOL", 1150.9, 1500.0, 'M', 3);
-INSERT INTO tb_personagem(nome, ataque, defesa, genero, classe_id) VALUES ("JUPITER", 1000.0, 250.33, 'O', 1);
-INSERT INTO tb_personagem(nome, ataque, defesa, genero, classe_id) VALUES ("SATURNO", 2266.33, 3000.0, 'F', 2);
-INSERT INTO tb_personagem(nome, ataque, defesa, genero, classe_id) VALUES ("PLUTÃO", 1444.15, 999.3, 'O', 5);
-INSERT INTO tb_personagem(nome, ataque, defesa, genero, classe_id) VALUES ("VENUS", 2247.57, 1172.33, 'M', 3);
-INSERT INTO tb_personagem(nome, ataque, defesa, genero, classe_id) VALUES ("MERCURIO", 1127.0, 1142.33, 'F', 1);
-INSERT INTO tb_personagem(nome, ataque, defesa, genero, classe_id) VALUES ("URANO", 1377.75, 844.33, 'O', 5);
+INSERT INTO tb_personagem (nome, ataque, defesa, genero, classe_id) VALUES ("Cher", 3333.33, 2000.0, 'F', 3);
+INSERT INTO tb_personagem (nome, ataque, defesa, genero, classe_id) VALUES ("Flor", 1150.9, 1500.0, 'M', 4);
+INSERT INTO tb_personagem (nome, ataque, defesa, genero, classe_id) VALUES ("Jade", 1000.0, 250.33, 'O', 3);
+INSERT INTO tb_personagem (nome, ataque, defesa, genero, classe_id) VALUES ("Lozzan", 2266.33, 3000.0, 'F', 1);
+INSERT INTO tb_personagem (nome, ataque, defesa, genero, classe_id) VALUES ("Hoop", 1444.15, 999.3, 'O', 2);
+INSERT INTO tb_personagem (nome, ataque, defesa, genero, classe_id) VALUES ("Kaya", 2247.57, 1172.33, 'M', 3);
+INSERT INTO tb_personagem (nome, ataque, defesa, genero, classe_id) VALUES ("Léia", 1127.0, 1142.33, 'F', 1);
+INSERT INTO tb_personagem (nome, ataque, defesa, genero, classe_id) VALUES ("Maia", 1377.75, 844.33, 'O', 5);
 
 SELECT * FROM tb_personagem WHERE ataque > 2000;
 SELECT * FROM tb_personagem WHERE defesa > 1000 AND defesa < 2000;
